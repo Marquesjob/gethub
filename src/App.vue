9fb691b675bcc25a26141dec65201d4f59f101c3
Marquesjob <patrickmarques.job@hotmail.com>
2022-07-13 20:12:04 -0300

    - Breakpoint res: 480px

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,40 @@ export default {
   max-width: 1200px;
   margin-bottom: 100px
 }
+
+/* Os breakpoints de todos os componentes estarão no componente principal */
+
+@media (max-width: 480px) {
+
+  .container {
+    width: 300px;
+  }
+
+  .nav {
+    margin-top: 25px;
+    display: flex;
+    flex-direction: column;
+  }
+ 
+  .nav__input {
+    width: 100%;
+    padding: 0;
+    padding-left: 20px;
+    margin: 0;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .nav__button {
+    width: 100%;
+    background: none;
+  }
+
+  p {
+    text-align: center;
+    margin-top: 10px;
+  }
+
+}
+
+
 </style>
